08c5ec1b7564ca5b7708a7f5885934e81ddf19d4
Maxwell Pereira <maxwell.pereira@sympla.com.br>
2019-05-31 13:19:17 -0300

    Criando o front

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,121 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<h1 class="text-center mt-5 mb-5">API Laravel</h1>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">		
+				<p><input type="text" class="form-control" v-model="busca" placeholder="Digite aqui para fazer sua busca do poduto" ></p>
+				<table class="table table-striped">
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">Name</th>
+							<th scope="col">Description</th>
+							<th scope="col">Price</th>
+							<th scope="col" class="text-center">Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="(prod, indice) in dadosSalvos" v-bind:key="indice">
+							<th scope="row">{{ indice }}</th>
+							<td>{{ prod.name }}</td>
+							<td>{{ prod.description }}</td>
+							<td>{{ prod.price }}</td>
+							<td class="text-center">
+								<button class="btn btn-secondary mr-1">edit</button>
+								<button class="btn btn-danger">delete</button>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import _ from 'lodash';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	data() {
+		return {
+			info: null,
+			produtos: null,
+			token: null,
+			busca: ''
+		};
+	},
+	created() {
+		this.login();	
+	},
+	computed: {
+		dadosSalvos() {			
+			let self = this;
+			return _.filter(this.produtos, function(produtos) {				
+				var busca = self.busca.toLowerCase(); // pega os dados dos campos				
+                return produtos.name.toLowerCase().indexOf(busca) >= 0;
+            });
+
+		},
+	},
+	methods: {
+		login() {
+			
+			axios({
+				method: 'post', // verbo http
+				url: 'http://localhost:8001/public/api/login', // url
+				data: {
+					email: 'user@user.com',
+					password: 'secret'
+				}
+			})
+			.then(response => {
+				this.token = response.data.token;
+				this.buscaProdutos();
+			})
+			.catch(error => {
+				console.log("Erro ao fazer login", error)
+			})
+		},
+		buscaProdutos() {
+
+
+			let config = {
+				headers: {
+					'Authorization': 'Bearer ' + this.token
+				}
+			}
+
+			// let bodyParameters = {
+			//     product: idProduct
+			// }
+
+			axios
+			.get(
+				'http://localhost:8001/public/api/products',
+				// bodyParameters,
+				config
+			)
+			.then( (response) => {
+				console.log(response.data.data);
+				this.produtos = response.data.data;
+
+			})
+			.catch( (error) => {
+				console.log(error);
+
+			});
+
+		}
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+	
 </style>
+
